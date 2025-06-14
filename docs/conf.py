a459e0df649b3b5de7ae3cdfce9e79ef3e46ae48
saadelkhader <saadlkhder3@gmail.com>
2025-06-14 16:58:05 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,31 +1,22 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'Analyse et Prédiction Temporelle du NDVI pour la Région Fès-Meknès'
 copyright = '2025, SAAD ELKHADER  ET  ASMAE ELHAKIOUI'
 author = 'SAAD ELKHADER  ET  ASMAE ELHAKIOUI'
 
+# -- Imports -----------------------------------------------------------------
+import sphinx_rtd_theme
+
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = []
-
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-language = 'FR'
+language = 'fr'
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = "sphinx_rtd_theme"
-
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
 
-# Configuration pour nbsphinx
-nbsphinx_execute = 'auto' # Exécute les notebooks lors de la construction
+# -- nbsphinx execution (optional) -------------------------------------------
+nbsphinx_execute = 'auto'
+
